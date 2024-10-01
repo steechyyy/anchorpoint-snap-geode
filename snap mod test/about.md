ee1d5bb84fd0e299bbe357e3f5ed3e8e0c48eb82
fabiolino4k <91980836+fabiolino4k@users.noreply.github.com>
2024-10-01 18:20:53 +0200

    Add project files.

---
+++ snap mod test/about.md
@@ -0,0 +1,3 @@
+# Template Mod
+
+small qol mod that allows you to snap the anchor point to one of the corners of whatever you're warping by right-clicking
---
+++ snap mod test/src/main.cpp
@@ -0,0 +1,44 @@
+/**
+ * Include the Geode headers.
+ */
+#include <Geode/Geode.hpp>
+
+/**
+ * Brings cocos2d and all Geode namespaces to the current scope.
+ */
+using namespace geode::prelude;
+
+/**
+ * `$modify` lets you extend and modify GD's classes.
+ * To hook a function in Geode, simply $modify the class
+ * and write a new function definition with the signature of
+ * the function you want to hook.
+ *
+ * Here we use the overloaded `$modify` macro to set our own class name,
+ * so that we can use it for button callbacks.
+ *
+ * Notice the header being included, you *must* include the header for
+ * the class you are modifying, or you will get a compile error.
+ *
+ * Another way you could do this is like this:
+ *
+ * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
+ */
+
+#include <Geode/modify/MenuLayer.hpp>
+class $modify(MyMenuLayer, MenuLayer) {
+	bool init() {
+		if (!MenuLayer::init())
+			return false;
+
+		auto winSize = CCDirector::get()->getWinSize();
+
+		auto label = cocos2d::CCLabelBMFont::create("Hello world", "bigFont.fnt");
+		label->setPosition(winSize / 2);
+		this->addChild(label);
+
+		return true;
+	}
+
+
+};
